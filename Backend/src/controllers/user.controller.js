8f508296d7fbfee625b85ac0e7758fa6c9b745de
Ashtosh Yadav <yadav1902ashutosh@gmail.com>
2026-09-15 01:02:46 +0530

    Implement user authentication with access and refresh token generation, and update user routes

--- Backend/src/controllers/user.controller.js
+++ Backend/src/controllers/user.controller.js
@@ -3,6 +3,31 @@ import {ApiError} from "../utils/apiError.js";
 import {User} from "../models/user.model.js";
 import {ApiResponse} from "../utils/apiResponse.js";
 
+const generateAccessandRefreshTokens = async (userId) => {
+    try {
+
+        // finding the user in DB
+        const user = await User.findById(userId);
+
+        //generating the AccessToken for the user
+        const accessToken = user.generateAccessToken()
+
+        //generating the refreshToken for the user
+        const refreshToken = user.generateRefreshToken()
+
+        //saving the generated refreshToken for the user in DB and disabling the updation of all the other fields
+        user.refreshToken = refreshToken
+        await user.save({validateBeforeSave: false})
+
+        //returning the generated tokens
+        return {accessToken, refreshToken}
+
+    } catch (error) {
+        throw new ApiError(500, "Something went wrong while generating the refresh and access tokens")
+    }
+}
+
+
 const registerUser = asyncHandler(async (req, res) => {
 
 // get user details from the frontend request
@@ -47,10 +72,116 @@ if(!createdUser){
 }
 
 // returing the response to the frontend
-return res.stausd(201).json({
+return res.stausd(201).json(
     new ApiResonse(
         200, createdUser, "User was refgister successfully"
     )
-})   
+)   
 
 })
+
+
+const loginUser = asyncHandler(async (req, res) => {
+
+    // req body -> data
+
+    const {email,username, password} = {...req.body}
+
+    // check if the user has provided either email or username
+    if(!email && !username)
+    {
+        throw new ApiError(400, "Email or username is required");
+    }
+
+    // check if the user has provided password  
+    if(!password)
+    {
+        throw new ApiError(400, "Password is required");
+    }
+
+    // check if the user exists in the database
+
+    const user = await User.findOne({
+        $or: [{email}, {username}]
+    })
+
+    if(!user)
+    {
+        throw new ApiError(404, "User not found");
+    }
+
+    //check if the password is correct
+    const isPasswordValid = await user.isPasswordCorrect(password);
+
+    if(!isPasswordValid)
+    {
+        throw new ApiError(401, "Invalid password");
+    }
+
+    // generating and storing the access and refresh tokens for the user
+
+    const {accessToken, RefreshToken} = await generateAccessandRefreshTokens(user._id);
+
+    //querying the updated user from DB to return in the response and removing password and  refreshtoken from it
+
+    const loggedInUser = await User.findById(user._id).select("-password -refreshToken");
+
+    //configuring options for the cookies
+
+    const options = {
+        httpOnly: true,
+        secure: true
+    }
+
+
+    //returning the response
+    return res
+    .status(200)
+    .cookie("accessToken",accessToken,options)
+    .cookie("refreshToken", refreshToken, options)
+    .json(
+        new ApiResponse(
+            200,
+            {
+                user: loggedInUser, accessToken, refreshToken
+            },
+            "user logged in successfully"
+        )
+    )
+})
+
+const logoutUser = asyncHandler(async(req,res) =>{
+    
+    //updating the user and removing the refreshtoken from the database
+    await User.findByIdAndUpdate(
+        request.user._id,
+        {
+            $unset: {
+                refreshToken: 1
+            }
+        },
+        {
+            returnDocument: "after"
+        }
+    )
+
+    // consfiguring options for cookies
+    const options = {
+        httpOnly: true,
+        secure: true
+    }
+
+    //returning the response and clearing user cookies
+
+    return res
+    .status(200)
+    .clearCookie("accessToken", options)
+    .clearCookie("refreshToken", options)
+    .json(new ApiResponse(200, {}, "User Logged Out"))
+})
+
+export {
+    registerUser,
+    loginUser,
+    logoutUser,
+}
--- Backend/src/routes/user.routes.js
+++ Backend/src/routes/user.routes.js
@@ -1,7 +1,18 @@
 import {Router} from 'express';
-
+import {loginUser, registerUser, logoutUser} from "../controllers/user.controller.js"
 
 
 const router = Router()
+
+//setting routs for register users
+router.route("/register").post(registerUser)
+
+//setting routes for login users
+router.route("/login").post(loginUser)
+
+//setting secured routes using middleware services
+routerl.route("/logout").post(verifyJWT, logoutUser)
+
+
 
 export default router
